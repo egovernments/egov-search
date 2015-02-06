28838db8484b3b7da09f9a5f8f1758f464582ba8
rishi <rishi@egovernments.org>
2015-02-06 11:56:27 +0530

    Rishi | Katta - Initial commit for egov-search module

---
+++ src/main/java/org/egov/search/config/SearchConfig.java
@@ -0,0 +1,32 @@
+package org.egov.config;
+
+import java.util.Properties;
+
+public class SearchConfig {
+
+    private Properties properties;
+
+    public ApplicationConfig(Properties properties) {
+        this.properties = properties;
+    }
+
+    public String[] searchHosts() {
+        return properties.getProperty("search.hosts").split(",");
+    }
+
+    public int searchPort() {
+        return Integer.parseInt(properties.getProperty("search.port"));
+    }
+
+    public String searchClusterName() {
+        return properties.getProperty("search.clusterName");
+    }
+
+    public int searchShardsFor(String indexName) {
+        return Integer.parseInt(properties.getProperty(String.format("search.%s.shards", indexName)));
+    }
+
+    public int searchReplicasFor(String indexName) {
+        return Integer.parseInt(properties.getProperty(String.format("search.%s.replicas", indexName)));
+    }
+}
