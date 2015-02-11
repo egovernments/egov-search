a62064b13bed43216d6639d30484c2692c3cfb5e
Srivatsa Katta <vatsa.katta@gmail.com>
2015-02-11 12:31:19 +0530

    Katta - Abstracted out search service test

---
+++ src/test/java/org/egov/search/service/SearchServiceMultipleFiltersTest.java
@@ -0,0 +1,90 @@
+package org.egov.search.service;
+
+import org.egov.search.domain.Filters;
+import org.egov.search.domain.SearchResult;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.egov.search.domain.Filters.withAndFilters;
+import static org.egov.search.domain.Filters.withAndPlusNotFilters;
+import static org.egov.search.domain.Filters.withAndPlusOrFilters;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SearchServiceMultipleFiltersTest extends SearchServiceTest {
+
+    @Test
+    public void shouldSearchWithSingleFilter() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.mode", "INTERNET");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
+
+        assertThat(searchResult.documentCount(), is(3));
+        assertThat(complaintNumbers(searchResult), contains("299DIF", "751HFP", "696IDN"));
+    }
+
+    @Test
+    public void shouldSearchWithMultipleFilters() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.mode", "INTERNET");
+        andFilters.put("clauses.status", "REGISTERED");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
+
+        assertThat(searchResult.documentCount(), is(2));
+        assertThat(complaintNumbers(searchResult), contains("751HFP", "696IDN"));
+    }
+
+    @Test
+    public void shouldSearchWithMultipleFiltersIncludingPartialMatch() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.status", "REGISTERED");
+        andFilters.put("common.citizen.address", "jakkasandra");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
+        assertThat(searchResult.documentCount(), is(2));
+        assertThat(complaintNumbers(searchResult), contains("810FBE", "892JBP"));
+    }
+
+    @Test
+    public void shouldSearchWithAndPlusOrFilters() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.status", "REGISTERED");
+
+        Map<String, String> orFilters = new HashMap<>();
+        orFilters.put("searchable.title", "mosquito OR garbage");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndPlusOrFilters(andFilters, orFilters));
+        assertThat(searchResult.documentCount(), is(3));
+        assertThat(complaintNumbers(searchResult), contains("810FBE","820LGN", "751HFP"));
+    }
+
+    @Test
+    public void shouldSearchWithOrFiltersOnSameField() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.status", "FORWARDED OR COMPLETED");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndFilters(andFilters));
+        assertThat(searchResult.documentCount(), is(2));
+        assertThat(complaintNumbers(searchResult), contains("299DIF","873GBH"));
+    }
+
+    @Test
+    public void shouldSearchWithAndPlusNotInFilter() {
+        Map<String, String> andFilters = new HashMap<>();
+        andFilters.put("clauses.status", "REGISTERED");
+
+        Map<String, String> notInFilters = new HashMap<>();
+        notInFilters.put("clauses.mode", "CITIZEN");
+
+        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndPlusNotFilters(andFilters, notInFilters));
+        assertThat(searchResult.documentCount(), is(2));
+        assertThat(complaintNumbers(searchResult), contains("751HFP","696IDN"));
+    }
+
+}
--- src/test/java/org/egov/search/service/SearchServiceTest.java
+++ src/test/java/org/egov/search/service/SearchServiceTest.java
@@ -2,38 +2,27 @@ package org.egov.search.service;
 
 import org.egov.search.AbstractNodeIntegrationTest;
 import org.egov.search.config.SearchConfig;
-import org.egov.search.domain.Filters;
 import org.egov.search.domain.SearchResult;
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.jayway.jsonpath.JsonPath.read;
 import static java.lang.String.format;
-import static java.util.Arrays.asList;
-import static org.egov.search.domain.Filters.withAndFilters;
-import static org.egov.search.domain.Filters.withAndPlusNotFilters;
-import static org.egov.search.domain.Filters.withAndPlusOrFilters;
 import static org.egov.search.util.Classpath.readAsString;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
-public class SearchServiceTest extends AbstractNodeIntegrationTest {
+public abstract class SearchServiceTest extends AbstractNodeIntegrationTest {
 
-    private String indexName;
+    protected String indexName;
+    protected String indexType;
+
     private ElasticSearchClient elasticSearchClient;
-
+    protected SearchService searchService;
     @Mock
     private SearchConfig searchConfig;
-    private SearchService searchService;
-    private String indexType;
 
     @Before
     public void before() {
@@ -51,75 +40,6 @@ public class SearchServiceTest extends AbstractNodeIntegrationTest {
         indexPGRdata();
     }
 
-    @Test
-    public void shouldSearchWithSingleFilter() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.mode", "INTERNET");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
-
-        assertThat(searchResult.documentCount(), is(3));
-        assertThat(complaintNumbers(searchResult), contains("299DIF", "751HFP", "696IDN"));
-    }
-
-    @Test
-    public void shouldSearchWithMultipleFilters() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.mode", "INTERNET");
-        andFilters.put("clauses.status", "REGISTERED");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
-
-        assertThat(searchResult.documentCount(), is(2));
-        assertThat(complaintNumbers(searchResult), contains("751HFP", "696IDN"));
-    }
-
-    @Test
-    public void shouldSearchWithMultipleFiltersIncludingPartialMatch() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.status", "REGISTERED");
-        andFilters.put("common.citizen.address", "jakkasandra");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), Filters.withAndFilters(andFilters));
-        assertThat(searchResult.documentCount(), is(2));
-        assertThat(complaintNumbers(searchResult), contains("810FBE", "892JBP"));
-    }
-
-    @Test
-    public void shouldSearchWithAndPlusOrFilters() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.status", "REGISTERED");
-
-        Map<String, String> orFilters = new HashMap<>();
-        orFilters.put("searchable.title", "mosquito OR garbage");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndPlusOrFilters(andFilters, orFilters));
-        assertThat(searchResult.documentCount(), is(3));
-        assertThat(complaintNumbers(searchResult), contains("810FBE","820LGN", "751HFP"));
-    }
-
-    @Test
-    public void shouldSearchWithOrFiltersOnSameField() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.status", "FORWARDED OR COMPLETED");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndFilters(andFilters));
-        assertThat(searchResult.documentCount(), is(2));
-        assertThat(complaintNumbers(searchResult), contains("299DIF","873GBH"));
-    }
-
-    @Test
-    public void shouldSearchWithAndPlusNotInFilter() {
-        Map<String, String> andFilters = new HashMap<>();
-        andFilters.put("clauses.status", "REGISTERED");
-
-        Map<String, String> notInFilters = new HashMap<>();
-        notInFilters.put("clauses.mode", "CITIZEN");
-
-        SearchResult searchResult = searchService.search(asList(indexName), asList(), withAndPlusNotFilters(andFilters, notInFilters));
-        assertThat(searchResult.documentCount(), is(2));
-        assertThat(complaintNumbers(searchResult), contains("751HFP","696IDN"));
-    }
 
     private void indexPGRdata() {
         for (int id = 203461; id <= 203471; id++) {
@@ -127,8 +47,8 @@ public class SearchServiceTest extends AbstractNodeIntegrationTest {
         }
         refreshIndices(indexName);
     }
-    
-    private List<String> complaintNumbers(SearchResult searchResult) {
+
+    protected List<String> complaintNumbers(SearchResult searchResult) {
         return read(searchResult.rawResponse(), "$..complaint_number");
     }
 }
