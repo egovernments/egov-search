d6ac853ecf21846fef0592b9eee454164045ec86
sreekanth.nair <sreekanth.nair@egovernments.org>
2016-03-24 18:27:50 +0530

    [EGOVES-1] Changed from commons lang 3 to 2 since lang 3 is not a dependency.

--- src/main/java/org/egov/search/config/SearchConfig.java
+++ src/main/java/org/egov/search/config/SearchConfig.java
@@ -44,7 +44,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
