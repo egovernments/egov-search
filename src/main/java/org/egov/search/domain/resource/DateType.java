1fc9b753d7e1a03a71f7590bacc14a60cede711b
satyamashish <satyam.ashish@egovernments.org>
2016-08-18 16:46:09 +0530

    [PHOENIX-4717] Elastic Search Date issue fix

--- src/main/java/org/egov/search/domain/resource/DateType.java
+++ src/main/java/org/egov/search/domain/resource/DateType.java
@@ -46,24 +46,15 @@ import java.util.Date;
 
 public class DateType extends Type {
 
-    public DateType(Field field) {
-        super(field);
-    }
+	public DateType(Field field) {
+		super(field);
+	}
 
-    @Override
-    public Object retrieveJsonValue(Object fieldValue) {
-        Calendar oldcl = Calendar.getInstance();
-        oldcl.setTime((Date) fieldValue);
-
-        Date now = new Date();
-        Calendar newcl = Calendar.getInstance();
-
-        newcl.setTime(now);
-        newcl.set(Calendar.DATE, oldcl.get(Calendar.DATE));
-        newcl.set(Calendar.MONTH, oldcl.get(Calendar.MONTH));
-        newcl.set(Calendar.YEAR, oldcl.get(Calendar.YEAR));
-
-        return newcl.getTime();
-    }
+	@Override
+	public Object retrieveJsonValue(Object fieldValue) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime((Date) fieldValue);
+		return calendar.getTime();
+	}
 
 }
